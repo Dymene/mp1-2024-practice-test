13513ce144230d789d8f970fff3e5883b45246f6
SARRANA <98034665+Dymene@users.noreply.github.com>
2024-12-19 09:43:51 +0300

    Update task1.c

--- 3824Б1ПР3/25 Фоменко Максим Валерьевич/task1.c
+++ 3824Б1ПР3/25 Фоменко Максим Валерьевич/task1.c
@@ -1,6 +1,14 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
-size_t task1(int A[], size_t n)
+size_t task1(int A[], size_t n) 
 {
-  return -1;
+    int count = 0;
+    for (int i = 1; i < n; i++) 
+    {
+        if (A[i] < A[i - 1]) 
+        {
+            count++;
+        }
+    }
+    return count;
 }
