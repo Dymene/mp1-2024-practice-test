dc5246651ca0d9597f717cd8d9cd28c20739ef05
SARRANA <98034665+Dymene@users.noreply.github.com>
2024-12-19 09:19:23 +0300

    Update task1.c

--- 3824Б1ПР3/25 Фоменко Максим Валерьевич/task1.c
+++ 3824Б1ПР3/25 Фоменко Максим Валерьевич/task1.c
@@ -2,5 +2,11 @@
 // Найти сколько чисел в массиве меньше своего левого соседа.
 size_t task1(int A[], size_t n)
 {
-  return -1;
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (A[i] < A[i - 1]) {
+            count++;
+        }
+    }
+    return count;
 }
