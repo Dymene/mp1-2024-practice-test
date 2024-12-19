8011ad6c6afab3759837b07c1224544c2876c7d3
SARRANA <98034665+Dymene@users.noreply.github.com>
2024-12-19 09:37:01 +0300

    Update task3.c

--- 3824Б1ПР3/25 Фоменко Максим Валерьевич/task3.c
+++ 3824Б1ПР3/25 Фоменко Максим Валерьевич/task3.c
@@ -1,6 +1,31 @@
 // Задача 3. Дано целое число N. Не используя функции стандартной библиотеки
 // языка С, перевести число в строковое представление в десятичном виде.
-char* task3(int N)
-{
-  return "task3";
+char* task3(int N) {
+    int isNegative = 0;
+    if (N < 0) {
+        isNegative = 1;
+        N = -N;
+    }
+
+    char* str = (char*)malloc(12);
+    int i = 0;
+
+    do {
+        str[i++] = (N % 10) + '0';
+        N /= 10;
+    } while (N > 0);
+
+    if (isNegative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '0';
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
+
+    return str;
 }
